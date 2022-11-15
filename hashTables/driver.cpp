ada0c8b63541f59caea873a3b6ca0bae67146c59
greg0rys <shenefeltgregory@gmail.com>
2022-11-14 21:46:01 -0800

    added cmake to hashtables

--- hashTables/driver.cpp
+++ hashTables/driver.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
+    cout << "HI";
 	return 0;
 }
 
--- hashTables/driver.h
+++ hashTables/driver.h
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <string>
 #include "table.h"
-
+#include "person.h"
 using namespace std;
 int main();
